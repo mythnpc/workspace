0feb835a97ea2c9750dc4d8e21428289d464910e
MythNPC <wrekk.game@gmail.com>
2020-11-10 22:48:25 +1100

    ClassLibrary for nuget

---
+++ ClassLibraryFramework/ClassLibraryFramework/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryFramework
+{
+    public class Class1
+    {
+        public String GetVersion()
+        {
+            return "1.0.2";
+        }
+    }
+}
