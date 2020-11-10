2847637459300a058fdcbca876fc8eae2fa33c2c
MythNPC <wrekk.game@gmail.com>
2020-11-10 22:46:40 +1100

    .Net Library proving ground

---
+++ ConsoleApp5/ConsLib/ConsThisLibrary.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsLib
+{
+    public class ConsThisLibrary
+    {
+        public String GetDiffVersion()
+        {
+            var temp = new Class1();
+            return temp.GetVersion();
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using ClassLibraryFramework;
+using ConsLib;
+
+class Solution
+{
+
+
+    static void Main(string[] args)
+    {
+        var temp = new Class1();
+        Console.WriteLine(temp.GetVersion());
+        var temp2 = new ConsThisLibrary();
+        Console.WriteLine(temp2.GetDiffVersion());
+        Console.Read();
+    }
+}
